1249a3d9cfe5d5ec41169ab3edb66db9835025d9
Chris Ibarra <chrisaaron184@gmail.com>
2023-06-16 10:16:09 -0700

    Initial Files Commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+const sayHello = (req, res, next) => {
+    res.send("Hello!");
+};
+
+const logging = (req, res, next) => {
+    console.log("A request has been made!");
+
+    next();
+};
+
+app.use(logging);
+app.use(sayHello);
+
+module.exports = app;
