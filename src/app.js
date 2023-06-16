b0e732ace952b74e0206b7ed64a54224fcdeca24
Chris Ibarra <chrisaaron184@gmail.com>
2023-06-16 11:40:07 -0700

    Access Query Parameters W/Conditions

--- src/app.js
+++ src/app.js
@@ -6,17 +6,35 @@ const morgan = require("morgan");
 // The Express package exports a function. When you invoke that function, you get a new Express application and assign it to a variable.
 const app = express();
 
-// Initial Middleware to respond 
-const sayHello = (req, res, next) => {
-    // The .send function sends the response through the server.
-    res.send("Hello!");
+// Initial Middleware to respond
+const sayHello = (req, res) => {
+    // Log the query parameters received in the request
+    console.log(req.query);
+
+    // Retrieve the value of the 'name' parameter from the query
+    const name = req.query.name;
+
+    // Construct the greeting content based on the presence of the 'name' parameter
+    const content = name ? `Hello, ${name}!` : 'Hello!';
+
+    // Send the content as the response to the client
+    res.send(content);
 };
+
 
 // Call app.morgan(dev) to return logs to console.
 app.use(morgan("dev"));
+
+// ---------- GET REQUESTS ---------- 
+
 /* When a client makes a GET request to "/hello" on the Express server, the sayHello function will be invoked, 
 allowing you to define the logic for handling that specific request. */
-app.get("/hello", sayHello);
+app.get('/hello', sayHello);
+
+app.get('/songs', (req, res) => {
+    const title = req.query.title;
+    res.send(title);
+});
 
 // Export the Express application to be used in the server.js file.
 module.exports = app;
