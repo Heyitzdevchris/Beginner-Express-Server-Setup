1b190557d279e5acf68d7b3e774f8ae86783cb43
Chris Ibarra <chrisaaron184@gmail.com>
2023-06-16 10:59:19 -0700

    Organized & Added More Pseudo Code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 // Require the Express package and assign it to a variable.
 const express = require("express");
-// The Express package exports a function. When you invoke that function, you get a new Express application and assign it to a variable.
+// Require the Morgan package and assign it to a variable.
 const morgan = require("morgan");
+
+// The Express package exports a function. When you invoke that function, you get a new Express application and assign it to a variable.
 const app = express();
 
 // Initial Middleware to respond 
@@ -10,6 +12,7 @@ const sayHello = (req, res, next) => {
     res.send("Hello!");
 };
 
+// Call morgan & sayHello to return to the user.
 app.use(morgan("dev"));
 app.use(sayHello);
 
